c2d93e026efd0ef4362e73085d8e3e00b70e3180
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2024-10-25 17:29:16 +0600

    chapter 3 example 3 done

---
+++ chapter-03/example-3.2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+   
+   int n;
+   n = 10;
+
+   if(n>0){
+      printf("This is a positive Number\n");
+   }else{
+      printf("This is a negative Number\n");
+   }
+
+   return 0;
+
+}
---
+++ chapter-03/example-3.3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+   int n;
+   n = 0;
+
+   if(n>0){
+      printf("This is a positive number\n");
+   }else if(n<0){
+      printf("This is a negative number\n");
+   }
+   else if (n == 0)
+   {
+      printf("The number is zero !\n");
+   }
+
+   return 0;
+}
