2b9749a0afca1b1d1997389d7274d7549eeb6ab0
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2024-10-25 17:13:08 +0600

    chapter-02 done

---
+++ chapter-02/example-12.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+   // test program - Comment 1
+   printf("Hello ");
+   /* We have printed Hello, Now we shall print World. Note that this is a multi-line comment */
+
+   printf("World "); // printed World.
+
+   return 0;
+}
